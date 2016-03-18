8150baa70e26ada1ccb83c44ba1624f1c99fd5c0
Nicole <nicolecarpenterusa@gmail.com>
2016-03-18 11:41:56 -0500

    fix alignment and remove redundant if/else statements

--- Sources/Specs/dictionaryManagerSpec.swift
+++ Sources/Specs/dictionaryManagerSpec.swift
@@ -2,22 +2,22 @@ import Dictionary
 import Swiftest
 
 class DictionaryManagerSpec: Swiftest.Spec {
-  	let spec = describe("dictionary logic") {
-        let dm = DictionaryManager()
+	let spec = describe("dictionary logic") {
+	let dm = DictionaryManager()
 
-        describe("#getRandomIndex") {
-        	it("returns a random number through dictionary length") {
-                	let index = dm.getRandomIndex()
-                	expect(index).to.beLessThan(words.count)
-           	}
-        }
+		describe("#getRandomIndex") {
+			it("returns a random number through dictionary length") {
+				let index = dm.getRandomIndex()
+				expect(index).to.beLessThan(words.count)
+			}
+		}
 
-        describe("#getRandomWord") {
-        	it("returns a random word from the dictionary") {
-                	let gameWords = words
-        		let word = dm.getRandomWord()
-        			expect(gameWords).to.contain(word)
-        		}
-        	}
-  	}
+		describe("#getRandomWord") {
+			it("returns a random word from the dictionary") {
+				let gameWords = words
+				let word = dm.getRandomWord()
+				expect(gameWords).to.contain(word)
+			}
+		}
+	}
 }
